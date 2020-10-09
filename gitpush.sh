c9f576b83ac0643152dce5c3cb629665e13ef16a
jpav0002 <jpav0002@red.ujaen.es>
2020-10-09 13:34:01 +0200

    Actualizando el sh script

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 echo "Añadiendo datos a git"
 git add Data_files
 echo "Realizando commits"
-git commit -m "Commit ${1}"
+git commit -m "Automatic Commit ${1}"
 echo "Realizando push"
 git push
 echo "Datos listos en repositorio"
