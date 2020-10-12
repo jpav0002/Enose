622f3fabe8028d78cccffcd0b6064e9ec3bf8ef0
jpav0002 <jpav0002@red.ujaen.es>
2020-10-12 18:08:36 +0200

    Adding .gitignore and final version of Smeasure.py

--- Smeasure.py
+++ Smeasure.py
@@ -7,44 +7,40 @@ from subprocess import Popen
 
 
 def upload_data():
-
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y_%H%M%S")
     Popen('./gitpush.sh %s' % (str(dt_string),), shell=True)
     return 0
 
 
-def create_csv(csv_data, muestras):
-
+def create_csv(csv_data):
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y_%H%M%S")
     file_name = "mediciones_" + str(dt_string) + ".csv"
-    path = "/Users/jpav/Documents/Scripts/Data_files/" + str(file_name)
+    path = "/Users/jpav/Documents/Enose/Data_files/" + str(file_name)
     f = open(path, "w")
-    f.write("Ch,Med1,Med2,Med3,Med4,Med5,Med6,Med7,Med8,Med9,Med10")
+
+    f.write(",,48,48,48,48,49,49,49,49,4B,4B,4B,4B")
+    f.write("\n")
+    f.write(",,0,1,2,3,0,1,2,3,0,1,2,3")
+    f.write("\n")
+    f.write(
+        "Temperatura,Humedad,SP3S-AQ2-01,TGS832-A00,TGS822,4 ó 1,NA,SK25F,NA,SB-51-00,4 ó 1,SP-31-00,TGS2602-B00,TGS2620-C00")
     f.write("\n")
 
     for file_data in csv_data:
 
         n = 0
-        i = 0
-        chain = str(i) + ","
-
-        if len(file_data) > muestras:
-            div = len(file_data) / 4
-        else:
-            div = muestras
+        chain = ""
+        size = len(file_data)
 
         for data in file_data:
 
             chain += str(data) + ","
             n += 1
-            if n % div == 0:
+            if n == size:
                 f.write(chain)
                 f.write("\n")
-                i += 1
-                chain = str(i) + ","
-                n = 0
 
     return 0
 
@@ -67,13 +63,6 @@ def leer_serial(data, ser):
 
 
 def mediciones():
-
-    T = []
-    H = []
-    adc0 = []
-    adc1 = []
-    adc2 = []
-
     tiempo = datetime.now()
 
     ser = serial.Serial(
@@ -95,56 +84,43 @@ def mediciones():
 
     minuto_inicio = tiempo.minute
 
-    while tiempo.minute - minuto_inicio < 2:
-        print("Muestra: " + str(muestras + 1) + " por 2 minutos")
+    csv_data = []
 
-        ser.write('t'.encode())
+    while tiempo.minute - minuto_inicio < 2:
+
+        print("Muestra: " + str(muestras + 1) + " por 1 minutos")
+
+        ser.write('T'.encode())
         time.sleep(1)
-        T = T + leer_serial(1, ser)
+        T = leer_serial(1, ser)
 
         ser.write('H'.encode())
-        H = H + leer_serial(1, ser)
+        H = leer_serial(1, ser)
 
         ser.write('0'.encode())
-        adc0 = adc0 + leer_serial(4, ser)
+        adc0 = leer_serial(4, ser)
 
         ser.write('1'.encode())
-        adc1 = adc1 + leer_serial(4, ser)
+        adc1 = leer_serial(4, ser)
 
         ser.write('2'.encode())
-        adc2 = adc2 + leer_serial(4, ser)
+        adc2 = leer_serial(4, ser)
 
-        muestras += 1
         tiempo = datetime.now()
 
-    adcs = [adc0, adc1, adc2]
-    adcs_new = [[]] * 3
-    index = 0
+        csv_data.append(T + H + adc0 + adc1 + adc2)
+        muestras += 1
 
-    for i in adcs:
-        adcx_0 = i[0::4]
-        adcx_1 = i[1::4]
-        adcx_2 = i[2::4]
-        adcx_3 = i[3::4]
-        adcs_new[index] = adcx_0 + adcx_1 + adcx_2 + adcx_3
-        index += 1
-
-    adc0 = adcs_new[0]
-    adc1 = adcs_new[1]
-    adc2 = adcs_new[2]
-
-    csv_data = [T, H, adc0, adc1, adc2]
-    create_csv(csv_data, muestras)
+    create_csv(csv_data)
     upload_data()
 
 
 def main():
     sched = BlockingScheduler()
 
-    #    sched.add_job(mediciones, 'cron', day_of_week='mon-sun', hour='9,13,17,21')
-    sched.add_job(mediciones, 'cron', day_of_week='mon-sun', hour='10', minute='33,36')
+    sched.add_job(mediciones, 'cron', day_of_week='mon-sun', hour='9,13,17,21')
+#    sched.add_job(mediciones, 'cron', day_of_week='mon-sun', hour='18', minute='00,02')
     sched.start()
-
 
 if __name__ == "__main__":
     main()
