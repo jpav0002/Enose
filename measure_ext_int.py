18f045872afaeb14ef9e48b5109638c8b8046790
jpav0002 <jpav0002@red.ujaen.es>
2021-05-27 13:43:34 +0200

    Automatic Commit 27052021_134334

--- measure_ext_int.py
+++ measure_ext_int.py
@@ -161,7 +161,7 @@ def main():
     
     sched = BlockingScheduler()
 
-    sched.add_job(mediciones(), 'cron', day_of_week='mon-sun', hour='0-23', minute="0,20,40")
+    sched.add_job(mediciones, 'cron', day_of_week='mon-sun', hour='0-23', minute="0,20,40")
     sched.start()
 #    mediciones()
 
